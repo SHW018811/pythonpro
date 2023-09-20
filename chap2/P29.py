dc19bd551d4627e4109b464b10572dd4a297b3f6
HeeWon <hewon0812@naver.com>
2023-09-20 02:50:20 +0000

    chap2 python file added

---
+++ chap2/P29.py
@@ -0,0 +1,9 @@
+name = input("What's your name? ")
+age = int(input("And how old are you? "))
+weigh = int(input("Okay, last question. How many pounds do you weigh?"))
+
+save = age/7
+save2 = age * 31536000
+
+print("Did you know that you're just",save,"in dog years?")
+print("\nBut you're also over",save2,"seconds old.")
---
+++ chap2/P61.py
@@ -0,0 +1,16 @@
+
+print("\n\t\t\tTrust Fund Buddy\n")
+
+print("Totals your monthly spending so that your trust fund doesn't run out\n<and you're forced to get a real job>.\n\n Please enter the requested, monthly costs.  Since you're rich, ignore pennies and use only dollar amounts.\n\n")
+
+Lam = int(input("Lamborghini Tune-Ups: "))
+Man = int(input("Manhattan Apartment : "))
+Jet = int(input("Private Jet Rental : "))
+Gift = int(input("Gifts : "))
+Din = int(input("Dinning Out : "))
+Stf = int(input("Staff <butlers, chef, driver, assistant>: "))
+Gru = int(input("Personal Guru and Coach: "))
+Com = int(input("Computer Games: "))
+
+print("\nGrand Total :",Lam+Man+Jet+Gift+Din+Stf+Gru+Com,"\n\n")
+print("Press the enter key to exit.")

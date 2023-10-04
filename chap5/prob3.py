5e26db0e5c08aaf9db7a83cbcedf09605f435f5b
HeeWon <hewon0812@naver.com>
2023-10-04 02:47:03 +0000

    Final

--- chap5/prob3.py
+++ chap5/prob3.py
@@ -1,17 +1,40 @@
 import random
 
+a = False
+b = False
 print("Guess the Word!!!")
 print("In this game, the program selects a word at random, and the player's objective is to guess the chosen word.\n\n")
 
 words = ("difficult", "banana", "apple", "python", "daegu", "catholic", "university")
 rword = random.choice(words)
 cnt = len(rword)
-
+guessword = ["_" for i in range(cnt)]
 print(guessword)
+print("Length of the selected word:",cnt)
+print(rword)
+while True:
+   if cnt == 0 : break
+   print("Remaining attemps:",cnt)
+   print("Current guessed word: ",end = '')
+   for i in range(len(rword)):
+       print(guessword[i],end=' ')
+   print()
+   inguess = input("Guess a letter: ")
 
-#print("Length of the selected word:",cnt)
-#while True:
-#   if cnt == 0 : break
-#   print("Remaining attemps:",cnt)
-#   print("Current guessed word:",guessword)
+   #right -> change
+   for i in range(len(rword)):
+       if(inguess == rword[i]):
+           guessword[i] = inguess
+           a = True
+   if a != True:
+       cnt -= 1
+   a = False
     
+   if "_" in guessword:
+        continue
+   else :
+      b = True
+      print("Congratulations! You guessed the word:",rword)
+      break
+if b == False : print("Incorrect guess.\nYou've used up all your attempts. The correct ord was python.")
+#no -> cnt -- kee   
